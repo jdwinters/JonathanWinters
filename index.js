133bb7cb09ef3210df2e9d292e2c614273d6561b
Jon <wintersfile@gmail.com>
2019-06-22 23:53:38 -0700

    stupid mistake

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ if(process.env.NODE_ENV === 'production'){
 
 	const path = require('path');
 	app.get('*', (req, res) => {
-		res.sendFIle(path.resolve(__dirname, 'client', 'build', 'index.html'));
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
 	});
 }
 
