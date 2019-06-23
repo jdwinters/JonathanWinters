7050a3f0fae696a75bb9dca3bd2d60db1a1f7c61
Jon <wintersfile@gmail.com>
2019-06-22 19:23:39 -0700

    hopeful fix

--- index.js
+++ index.js
@@ -3,5 +3,14 @@ const passport = require('passport');
 
 const app = express();
 
+if(process.env.NODE_ENV === 'production'){
+	app.use(express.static('client/build'));
+
+	const path = require('path');
+	app.get('*', (req, res) => {
+		res.sendFIle(path.resolve(__dirname, 'client', 'build', 'index.html'));
+	});
+}
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
