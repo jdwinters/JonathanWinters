845657db61bc2da654dc96503ed3e244132a56ad
Jon <wintersfile@gmail.com>
2019-06-22 19:44:18 -0700

    test

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -6,9 +6,11 @@ const About = () => {
 	return(
 		<div className="center">
 			<h2  style={{color: 'white'}}>Hi There</h2>
-			<img style={{ height: '600px' }}src="https://www.grammy.com/sites/com/files/styles/image_landscape_hero/public/miguel-hero-168193507.jpg?itok=o-EKrRkB" />
 		</div>
 	);
 };
 
 export default About;
+
+
+//			<img style={{ height: '600px' }}src="https://www.grammy.com/sites/com/files/styles/image_landscape_hero/public/miguel-hero-168193507.jpg?itok=o-EKrRkB" />
